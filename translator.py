f56a3d34bdba79781efbf3e89ac5c78fbbe9bfd7
Nishant <cybercoder.nishant@gmail.com>
2023-04-24 02:28:12 +0100

    Improved code and added features

--- translator.py
+++ translator.py
@@ -1,4 +1,6 @@
-# Morse Code Translator Using Python. 
+import sys
+
+# Morse Code Translator Using Python.
 
 MORSE_DICT = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
               'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
@@ -10,56 +12,59 @@ MORSE_DICT = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '.
 # Defining a function to encrypt the string from the dict
 
 def encrypt(message):
-    cipher = ''
+    cipher = []
     for letter in message:
         if letter != ' ':
 
             # Looks at the dict and adds corresponding code and separates with a space (' ') for different chars
-            cipher += MORSE_DICT[
-                          letter] + ' '
+            cipher.append(MORSE_DICT[letter])
         else:
-            cipher += ' '  # 1 space = different letter; 2 spaces = different word
+            cipher.append('/')  # 1 space = different letter; 2 spaces = different word
 
-    return cipher
+    return ' '.join(cipher)
 
 
 # Defining a function decrypt the string from the code to English
 
 def decrypt(message):
-    message += ' '  # Adding an extra space at the end to access the last bit of code
-    decipher = ''
-    citext = ''
-    for letter in message:
+    decipher = []
+    for word in message.strip().split("/"):
+        constructed_word = []
+        for letter in word.strip().split(" "):
+            inv = dict([(MORSE_DICT[l], l) for l in MORSE_DICT])
 
-        if letter != ' ':  # Checking for space here - note how a missing space earlier ruined the whole code!
-            # Created a counter to keep track of spaces (initialising a counter before the for loop) -
-            # needed to be in the 'if' loop and not outside
-            i = 0
-            citext += letter  # Stores code of a single character
+            constructed_word.append(inv.get(letter, ''))
+        decipher.append(''.join(constructed_word))
 
+    return ' '.join(decipher).lower()
+
+
+def main(args):
+    if len(args) < 2:
+        print("Please enter a command!")
+        return
+
+    if args[1] == 'encrypt':
+        if len(args) == 3:
+            path = args[2]
+            with open(path, 'r') as f:
+                message = f.read()
         else:
+            message = input("Enter what you seek to encrypt: ").upper()
 
-            i += 1  # If i = 1, that indicates a new char
+        result = encrypt(message.upper())
+        print(result)
+    elif args[1] == 'decrypt':
+        if len(args) == 3:
+            path = args[2]
+            with open(path, 'r') as f:
+                message = f.read()
+        else:
+            message = input("Enter what you wish to decrypt: ").upper()
 
-            if i == 2:
-                decipher += ' '  # Adding space to indicate new words
-
-            else:
-                decipher += list(MORSE_DICT.keys())[list(MORSE_DICT.values()).index(citext)]
-                citext = ''
-
-    return decipher
-
-
-def main():
-    message = input("Enter what you seek to encrypt: ").upper()
-    result = encrypt(message.upper())
-    print(result)
-
-    message = input("Enter what you wish to decrypt: ")
-    result = decrypt(message)
-    print(result)
+        result = decrypt(message)
+        print(result)
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
